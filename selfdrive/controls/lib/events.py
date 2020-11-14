c78b47c73bfde319b9228b9583eeb922c60d34f2
ChaseCarHG <6711962+ChaseCarHG@users.noreply.github.com>
2020-11-14 00:07:17 -0500

    Update events.py
    * Optimized for dawn/dusk for Eon.

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -313,7 +313,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
 
   EventName.gasPressed: {
     ET.PRE_ENABLE: Alert(
-      "openpilot will not brake while gas pressed",
+      "OpenPilot will NOT control while gas pressed",
       "",
       AlertStatus.normal, AlertSize.small,
       Priority.LOWEST, VisualAlert.none, AudibleAlert.none, .0, .0, .1, creation_delay=1.),
@@ -387,8 +387,8 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
     ET.WARNING: Alert(
       "CHECK DRIVER FACE VISIBILITY",
       "Driver Monitoring Uncertain",
-      AlertStatus.normal, AlertSize.mid,
-      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.none, .4, 0., 1.5),
+      AlertStatus.normal, AlertSize.small,
+      Priority.LOW, VisualAlert.none, AudibleAlert.none, .4, 0., 10),
   },
 
   EventName.manualRestart: {
